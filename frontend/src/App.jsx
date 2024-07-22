0a29d2fc7d6e7fa98db8a2c3c51ae71db3caed07
Francesca993 <montinifrancesca993@gmail.com>
2024-07-22 12:48:27 +0200

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import header from "./components/header";
+
+function App() {
+  return (
+    <>
+      <h1>Ciao</h1>
+      <h2>ciaone</h2>
+      <header />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function header() {
+  return (
+    <div>
+      <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quia
+        quibusdam ea, nisi perferendis inventore fugiat quaerat placeat eum
+        pariatur distinctio delectus! Numquam, aut quo aliquam dolore ipsum quia
+        laboriosam.
+      </h3>
+      <h2>Addio</h2>
+    </div>
+  );
+}
