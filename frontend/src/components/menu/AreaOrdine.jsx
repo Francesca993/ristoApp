96186f8ae4afed4b25fa1b3c56ea45ba955d0e3f
Francesca993 <montinifrancesca993@gmail.com>
2024-07-29 11:57:59 +0200

    fetch nell'area ordine

--- frontend/src/components/menu/AreaOrdine.jsx
+++ frontend/src/components/menu/AreaOrdine.jsx
@@ -6,16 +6,37 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 
 export default function AreaOrdine() {
+  // CREATE (CRUD) -> Funzione per gestire l'aggiunta di un nuovo elemento
+  const handleAddItem = () => {
+    // Creiamo un nuovo oggetto elemento con il valore dell'input
+    const newItem = { name: inputValue };
+
+    // POST all'API per aggiungere il nuovo elemento
+    fetch("http://localhost:5401/ListaOrdine", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", // Specifica il tipo di contenuto come JSON
+      },
+      body: JSON.stringify(newItem), // Convertiamo il nuovo oggetto elemento in una stringa JSON
+    })
+      .then((response) => response.json()) // Convertiamo la risposta in formato JSON
+      .then((data) => setItems([...items, data])); // Aggiorniamo la lista degli elementi con il nuovo elemento
+
+    // Resettiamo il valore dell'input
+    setInputValue("");
+  };
   return (
     <Container>
       <Row>
         <Col>
-          <Button variant="secondary">Aggiungi al carrello</Button>
+          <Button variant="secondary" onClick={handleAddItem()}>
+            Aggiungi al carrello
+          </Button>
         </Col>
         <Col>
           <Form.Select aria-label="Default select example">
             <option>Quantità</option>
-            <option value="1">1</option>
+            <option value={"1"}>1</option>
             <option value="2">2</option>
             <option value="3">3</option>
             <option value="4">4</option>
